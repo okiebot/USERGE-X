28c9ef9d104f9c98da371ee4718069cc87202c76
LaloHaxSkids <66586111+LaloHaxSkids@users.noreply.github.com>
2021-05-08 11:57:11 -0700

    Update whois.py

--- userge/plugins/fun/whois.py
+++ userge/plugins/fun/whois.py
@@ -46,24 +46,14 @@ async def who_is(message: Message):
         return
     if from_user or from_chat is not None:
         pp_c = await message.client.get_profile_photos_count(from_user.id)
-        message_out_str = "<b>USER INFO:</b>\n\n"
-        message_out_str += f"<b>🗣 First Name:</b> <code>{from_user.first_name}</code>\n"
-        message_out_str += f"<b>🗣 Last Name:</b> <code>{from_user.last_name}</code>\n"
-        message_out_str += f"<b>👤 Username:</b> @{from_user.username}\n"
-        message_out_str += f"<b>🏢 DC ID:</b> <code>{from_user.dc_id}</code>\n"
-        message_out_str += f"<b>🤖 Is Bot:</b> <code>{from_user.is_bot}</code>\n"
-        message_out_str += f"<b>🚫 Is Restricted:</b> <code>{from_user.is_scam}</code>\n"
-        message_out_str += "<b>✅ Is Verified by Telegram:</b> "
-        message_out_str += f"<code>{from_user.is_verified}</code>\n"
-        message_out_str += f"<b>🕵️‍♂️ User ID:</b> <code>{from_user.id}</code>\n"
+        message_out_str += f"<b> Username:</b> @{from_user.username}\n"
+        message_out_str += f"<b> Is Restricted:</b> <code>{from_user.is_scam}</code>\n"
         message_out_str += f"<b>🖼 Profile Photos:</b> <code>{pp_c}</code>\n"
         try:
             cc_no = len(await message.client.get_common_chats(from_user.id))
         except BotMethodInvalid:
             pass
         else:
-            message_out_str += f"<b>👥 Common Chats:</b> <code>{cc_no}</code>\n"
-        message_out_str += f"<b>📝 Bio:</b> <code>{from_chat.bio}</code>\n\n"
         message_out_str += f"<b>👁 Last Seen:</b> <code>{from_user.status}</code>\n"
         message_out_str += "<b>🔗 Permanent Link To Profile:</b> "
         message_out_str += (
