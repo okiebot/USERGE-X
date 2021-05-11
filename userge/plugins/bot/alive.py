65e12f2bcee67b4c27d4715f69db11cfaa6465fd
LaloHaxSkids <66586111+LaloHaxSkids@users.noreply.github.com>
2021-05-11 09:52:11 -0700

    Update alive.py

--- userge/plugins/bot/alive.py
+++ userge/plugins/bot/alive.py
@@ -227,7 +227,7 @@ class Bot_Alive:
     @staticmethod
     def alive_info() -> str:
         alive_info_ = f"""
-<a href="https://t.me/joinchat/EodhGjP6GjkyYzgx"><b>SCUMZ</a>CLICK ME TO JOIN SCUMZ</b>
+<a href="https://t.me/joinchat/EodhGjP6GjkyYzgx"><b>   SCUMZ   </a>CLICK TO JOIN SCUMZ</b>
 
   ♦️   <b>Python :</b>    <code>v{versions.__python_version__}</code>
   ♦️   <b>Pyrogram :</b>    <code>v{versions.__pyro_version__}</code>
